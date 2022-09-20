3db9935bd74beec56b7e4712d32183474fa3cf04
haeyun jun <haeyun.jun@nuvi-labs.com>
2022-09-20 21:48:09 +0900

    :zap:

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+//craco.config.js
+const CracoAlias = require("craco-alias");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "tsconfig",
+        tsConfigPath: "tsconfig.path.json",
+      },
+    },
+  ],
+};
